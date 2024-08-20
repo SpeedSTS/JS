b7e09cf394035c12f61d899a411cd1f1edf25195
Kamil <speedway943@wp.pl>
2024-08-20 20:33:59 +0200

    Uzupełnienie dodawania wierszy do tabeli

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
-const product1 = {price: 10 };
-const product2 = {price: 20 };
+const product1 = {price: 10, title: "JS od podstaw" };
+const product2 = {price: 20, title: "PHP od podstaw"};
 const discount = 10;
 
 const total = product1.price + product2.price;
@@ -7,5 +7,16 @@ const totalWithDiscount = total - discount;
 
 document.querySelector('#total-price').innerHTML = total;
 const items = document.querySelector('#items');
-
-console.log(`Cena przed zniżką ${total} natomiast cena po zniżce ${totalWithDiscount}`);
+let counter = 1;
+// itemsContainer.insertAdjacentHTML - (alternatywa dla innerHTML)
+function addItem(item) { 
+    items.innerHTML += `
+                <tr>
+                <td>${counter++}</td>
+                <td>${item.title}</td>
+                <td>1</td>
+                <td>${item.price}</td>
+                </tr>`
+}
+addItem(product1);
+addItem(product2);
