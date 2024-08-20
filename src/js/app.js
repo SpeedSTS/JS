552668f70f99441fa6b5db640b5d1fa28718b7c1
Kamil <speedway943@wp.pl>
2024-08-20 18:59:33 +0200

    Pobranie elementu ze strony

--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,8 @@ const discount = 10;
 
 const total = product1.price + product2.price;
 const totalWithDiscount = total - discount;
+
+document.querySelector('#total-price').innerHTML = total;
+const items = document.querySelector('#items');
+
 console.log(`Cena przed zniżką ${total} natomiast cena po zniżce ${totalWithDiscount}`);
