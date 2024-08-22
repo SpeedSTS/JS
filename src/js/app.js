4950c5dfb6f51f40f4d41b1547335e697727534e
Kamil <speedway943@wp.pl>
2024-08-22 19:36:31 +0200

    Dodanie cheboxa z obsługą zniżki

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,7 @@
 const product1 = {price: 10, title: "JS od podstaw" };
 const product2 = {price: 20, title: "PHP od podstaw"};
 const discount = 10;
-
-const total = product1.price + product2.price;
-const totalWithDiscount = total - discount;
-
-document.querySelector('#total-price').innerHTML = total;
+let discountEnabled = false;
 const items = document.querySelector('#items');
 let counter = 1;
 // itemsContainer.insertAdjacentHTML - (alternatywa dla innerHTML)
@@ -20,3 +16,24 @@ function addItem(item) {
 }
 addItem(product1);
 addItem(product2);
+
+// możliwośc dodawania zniżki
+
+function addDiscount() {
+    discountEnabled = window.event.target.checked;
+    if(discount > 0) {
+        document.querySelector('#discount-amount').innerHTML = discount;
+        document.querySelector('#discount').classList.toggle('hidden'); //classList.toggle (toggle - odwróć)
+    }
+    calculatePrice();
+}
+
+function calculatePrice() {
+    let total = product1.price + product2.price;
+    if(discountEnabled) {
+        total -= discount;
+    }
+    document.querySelector('#total-price').innerHTML = total;
+}
+
+calculatePrice();
