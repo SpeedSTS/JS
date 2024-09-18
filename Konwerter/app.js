52f7361ab6600b2086bdebf28f6f0a89fc130a9f
Kamil Stanicki <Kamil.Stanicki@komputronik.pl>
2024-09-18 15:44:59 +0200

    konwerter

---
+++ Konwerter/app.js
@@ -0,0 +1,18 @@
+// pobieranie elementów
+const item = document.querySelector('#check');
+console.log(item);
+
+// funkcje
+
+function checkBox(e) {
+
+}
+
+// podpięcie listenerów
+
+item.addEventListener('click', checkBox);
+
+
+
+
+
