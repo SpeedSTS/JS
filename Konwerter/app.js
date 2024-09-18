89734e5edc803b4f91ef3600a2ad36d6643fce76
Kamil <speedway943@wp.pl>
2024-09-18 19:19:21 +0200

    Poprawa działania klasy check

--- Konwerter/app.js
+++ Konwerter/app.js
@@ -1,18 +1,21 @@
 // pobieranie elementów
 const item = document.querySelector('#check');
+let units;
 
 // funkcje
 function checkBox(e) {
-
+    for(i = 0; i < item.children.length; i++)
+    {
+        item.children[i].classList.remove('check');
+    }
     if(e.target.tagName === 'BUTTON')
     {
         e.target.classList.add('check');
     }
-    
+    units = e.target.dataset.name;
 }
 
 // podpięcie listenerów
-
 item.addEventListener('click', checkBox);
 
 
